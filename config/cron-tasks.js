c1eab67d7f50a20e1c8bf718f6cc662a6ac29c21
unknown <shashankh1092@gmail.com>
2023-07-01 14:34:20 +0530

    api category  ADDED

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -5,14 +5,14 @@ module.exports = {
   myJob: {
     task: async () => {
       try {
-        const response = await axios.get('https://simple-one-id45.onrender.com/api/products/1'); // Replace with your desired URL
+        const response = await axios.get('https://simple-one-id45.onrender.com/api/products/1',"https://simple-one-id45.onrender.com/api/category/1"); // Replace with your desired URL
         console.log('HTTP GET request successful:', response.data);
       } catch (error) {
         console.error('Error making HTTP GET request:', error);
       }
     },
     options: {
-      cronTime: '0 */5 * * * *', // Run every 5 minutes
+      cronTime: '* * * * *', // Run every 1 minutes
     },
   },
 };
